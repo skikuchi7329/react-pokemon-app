c2a9e8331f50cf9a2d80545f9ac063f5f429ce17
Shohei Kikuchi <killchance2256@gmail.com>
2024-05-26 02:33:18 +0900

    add loadPokemon, ２０種類のポケモンのさらに個別にデータを取得

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { getAllPokemon } from "./utils/pokemon";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
+      //全てのポケモンデータの取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      //各ポケモンの詳細データを取得
+      loadPokemon(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
 
   return (
     <div className="App">
