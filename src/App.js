dea8737e8d2f29a606d589c79ae8a9ae028b4c79
Shohei Kikuchi <killchance2256@gmail.com>
2024-05-25 20:32:28 +0900

    ポケモンデータ取得とローディングの実装

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
-  return <div className="App"></div>;
+  const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <h1>ポケモンデータを取得しました</h1>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
